afe0ca59d1f494a291c36471095c40cb4b8cfbb3
Jch4ipas <jeremy.wuersch@gmail.com>
2024-09-24 10:11:43 +0200

    [fix]format document / description

--- script.js
+++ script.js
@@ -4,51 +4,49 @@ var APIKEYALREADYINSERT
 const todayDate = new Date();
 todayDate.toLocaleDateString();
 console.log(todayDate);
-if (paramdate == 'Thu Jan 01 1970 01:00:00 GMT+0100 (Central European Standard Time)' ){
+
+//si pas de date mettre date du jour
+if (paramdate == 'Thu Jan 01 1970 01:00:00 GMT+0100 (Central European Standard Time)') {
   paramdate = new Date(todayDate);
 }
 
 // endroit où inséré clé api
 function APIKEYINSERT() {
   var APIKEYALREADYINSERT = document.getElementById("apikeyinput").value;
-    localStorage.setItem('APIKEY', APIKEYALREADYINSERT)
-  document.location.href="/kata-APOD/"; 
+  localStorage.setItem('APIKEY', APIKEYALREADYINSERT)
+  document.location.href = "/kata-APOD/";
   console.log(APIKEYALREADYINSERT)
 }
 
 
-
-
-
 function updateinfo(paramdate) {
 
-    // Define the API URL
+  // Define the API URL
   window.history.pushState("", "title", "/kata-APOD/index.html?date=" + new Date(paramdate).toISOString().split('T')[0]);
-  const apiUrl = 'https://api.nasa.gov/planetary/apod?api_key=' + localStorage.getItem('APIKEY') + '&date=' + new Date(paramdate).toISOString().split('T')[0];
-// Make a GET request
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-    console.log(data.date)
 
-    if (data.media_type == "video") {
+  const apiUrl = 'https://api.nasa.gov/planetary/apod?api_key=' + localStorage.getItem('APIKEY') + '&date=' + new Date(paramdate).toISOString().split('T')[0];
+  // Make a GET request
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.media_type == "video") {
         //afficher vidéo et cacher image 
         document.getElementById("media-video").src = data.url;
         document.getElementById("media-image").style.display = 'none';
         document.getElementById("media-video").style.display = 'block';
-    } else {
+      } else {
         //afficher image et cacher vidéo
         document.getElementById("media-image").src = data.url;
         document.getElementById("media-video").style.display = 'none';
         document.getElementById("media-image").style.display = 'block';
-    }
-    let btn = document.getElementById("LA");
+      }
+      //ce sont les bouttons
+      let btn = document.getElementById("LA");
       btn.addEventListener("click", previousDay);
       let btn1 = document.getElementById("RA");
       btn1.addEventListener("click", tomorrowDay);
@@ -60,22 +58,20 @@ fetch(apiUrl)
       else {
         document.getElementById("RA").style.display = 'none';
       }
-      let btn1 = document.getElementById("RA");
-    console.log(btn)
-        btn1.addEventListener("click", tomorrowDay);
 
-    document.getElementById("image-date").innerHTML = data.date
-    document.getElementById("description").innerHTML = data.explanation
-    document.getElementById("image-title").innerHTML = data.title
+      //afficher tout
+      document.getElementById("image-date").innerHTML = data.date
+      document.getElementById("description").innerHTML = data.explanation
+      document.getElementById("image-title").innerHTML = data.title
       document.getElementById("copyright").innerHTML = data.copyright
       document.getElementById("linkimage").href = data.hdurl
 
-})
-  .catch(error => {
-    console.error('Error:', error);
-  });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 }
-
+//funcion pour aller au prochain jour et au jour d'avant
 function previousDay() {
   paramdate.setDate(paramdate.getDate() - 1);
   updateinfo(paramdate);
@@ -84,4 +80,5 @@ function tomorrowDay() {
   paramdate.setDate(paramdate.getDate() + 1);
   updateinfo(paramdate)
 }
+
 updateinfo(paramdate);
