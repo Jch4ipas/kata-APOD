fe40341230cd93aabc505eaedfef35d636c2e2d9
Jchaipas <jeremy.wuersch@gmail.com>
2025-07-01 12:02:01 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
 if (localStorage.getItem('APIKEY') == null) {
     document.location.href="/kata-APOD/newpage.html"; 
 }
-//ce script est pour si il n'y a pas de clé api que ça va sur la page ou on met la clé api
+//ce script fait une redirection si l'API key est null
